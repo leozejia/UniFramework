a45228a8efa511cd46df12839f1f83be8cc9abd1
leozejia <772618203@qq.com>
2020-12-17 17:52:54 +0800

    FirstTest

---
+++ UniFramework/Assets/QFrameworkLearn/HelloQF.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QFramework;
+
+public class HelloQF : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Log.I("Hello QF");
+
+        "I am leo".LogInfo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
