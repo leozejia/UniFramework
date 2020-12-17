c38d88562fc54851dd123e5b588117ef0e28ab88
leozejia <772618203@qq.com>
2020-12-17 18:53:52 +0800

    QFramework res ui

--- UniFramework/Assets/QFrameworkLearn/HelloQF.cs
+++ UniFramework/Assets/QFrameworkLearn/HelloQF.cs
@@ -5,12 +5,20 @@ using QFramework;
 
 public class HelloQF : MonoBehaviour
 {
+    ResLoader mResLoader = ResLoader.Allocate();
+
     // Start is called before the first frame update
     void Start()
     {
         Log.I("Hello QF");
 
         "I am leo".LogInfo();
+
+        mResLoader.Add2Load("TestObj",(succeed,res)=> { 
+            
+        });
+
+        mResLoader.LoadAsync();
     }
 
     // Update is called once per frame
---
+++ UniFramework/Assets/Scripts/Game/GameObject.Designer.cs
@@ -0,0 +1,14 @@
+// Generate Id:2e104a39-b638-4916-a3d8-0bf8bc7accdc
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace QFLearn
+{
+	public partial class GameObject
+	{
+		public UnityEngine.Transform Player;
+		public UnityEngine.Transform GameRoot;
+		public UnityEngine.Transform GameRoot2;
+	}
+}
