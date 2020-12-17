abb0704f0ae431ca3c255e6b62196235b8b7be6f
leozejia <772618203@qq.com>
2020-12-17 17:46:51 +0800

    QFramework

---
+++ UniFramework/Assets/UGUI/LButton.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LButton : Button
+{
+    public bool scaleAnim = false;
+    public Vector3 clickDownScale = new Vector3(0.95f, 0.95f, 0.95f);
+    public Vector3 normalScale = Vector3.one;
+
+    public bool sound = false;
+    public AudioClip clickSound = null;
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+
+        if (scaleAnim)
+            transform.localScale = clickDownScale;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        base.OnPointerUp(eventData);
+
+        if (scaleAnim)
+            transform.localScale = normalScale;
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+
+        if (sound && clickSound != null)
+        {
+            Debug.LogError("PlaySound");
+        }
+    }
+}
+
+[CustomEditor(typeof(LButton))]
+public class LButtonEditor : UnityEditor.UI.ButtonEditor
+{
+    LButton lButton;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        lButton = (LButton)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.Space();
+        serializedObject.Update();
+
+        var prop_ScaleAnim = this.serializedObject.FindProperty("scaleAnim");
+        EditorGUILayout.PropertyField(prop_ScaleAnim);
+        if (prop_ScaleAnim.boolValue)
+        {
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("clickDownScale"));
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("normalScale"));
+        }
+
+        var prop_Sound = this.serializedObject.FindProperty("sound");
+        EditorGUILayout.PropertyField(prop_Sound);
+        if (prop_Sound.boolValue)
+        {
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("clickSound"));
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ UniFramework/Assets/UGUI/ToggleTEST.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleTEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Toggle>().onValueChanged.AddListener(OnToggleSelect);
+    }
+
+    private void OnToggleSelect(bool arg0)
+    {
+        if (arg0)
+            Debug.Log(this.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
